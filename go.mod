acf18d5a7bb9457e7d83ed421f33dabaeb0adaff
Casey White <cwhite@localhost.localdomain>
2023-08-02 12:10:21 -0400

    sdl overwrite

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/casey-white/go-chip-8
 
 go 1.20
 
-require github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+require github.com/veandco/go-sdl2 v0.4.35
--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("SDL Tutorial", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 640, 480, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{0, 0, 200, 200}
+	color := sdl.Color{255, 0, 0, 255}
+	pixel := sdl.MapRGBA(surface.Format, color.R, color.G, color.G, color.A)
+	surface.FillRect(&rect, pixel)
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			}
+		}
+	}
+
 }
